f460b92af2c1927c844842bd5af66e676bd4faf6
asa1984 <satoasa9913@gmail.com>
2024-01-08 21:43:02 +0900

    add scripts

--- flake.nix
+++ flake.nix
@@ -2,40 +2,62 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import inputs.nixpkgs {inherit system;};
-      craneLib = inputs.crane.lib.${system};
       dev-deps = with pkgs; [
         nodejs-slim_20
         bun
         nodePackages.pnpm
         nodePackages.wrangler
+        nodePackages.vercel
         biome
         sqlite
       ];
+      ci-deps = with pkgs; [
+        nodejs-slim_20
+        nodePackages.pnpm
+        nodePackages.wrangler
+      ];
+      scripts = with pkgs; [
+        (writeScriptBin "install" ''
+          ${nodePackages.pnpm}/bin/pnpm install
+        '')
+        (writeScriptBin "build" ''
+          ${nodePackages.pnpm}/bin/pnpm run gen
+          ${nodePackages.pnpm}/bin/pnpm run build
+        '')
+      ];
     in {
-      devShell = pkgs.mkShell {
-        packages = dev-deps;
-      };
-      packages = {
-        cli = craneLib.buildPackage {
-          src = craneLib.cleanCargoSource (craneLib.path ./packages/cli);
+      devShells = rec {
+        default = dev;
+        dev = pkgs.mkShell {
+          packages = dev-deps ++ scripts;
         };
-
-        default = pkgs.rustPlatform.buildRustPackage {
+        ci = pkgs.mkShell {
+          packages = ci-deps ++ scripts;
+        };
+      };
+      packages = rec {
+        default = cli;
+        cli = pkgs.rustPlatform.buildRustPackage {
           name = "cli";
-
           src = ./packages/cli;
-
           cargoLock = {lockFile = ./packages/cli/Cargo.lock;};
         };
+
+        # Scripts
+        install = with pkgs;
+          writescriptbin "install" ''
+            ${nodepackages.pnpm}/bin/pnpm install
+          '';
+        build = with pkgs;
+          writeScriptBin "build" ''
+            ${nodePackages.pnpm}/bin/pnpm run gen
+            ${nodePackages.pnpm}/bin/pnpm run build
+          '';
       };
     });
 }
