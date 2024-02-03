d296bbd1cd2dda81aec95b6db8e0e31808f1859c
asa1984 <satoasa9913@gmail.com>
2024-02-03 12:10:02 +0900

    fix blog's date diff

--- packages/frontend/src/app/(routes)/blog/_components/LinkCard.tsx
+++ packages/frontend/src/app/(routes)/blog/_components/LinkCard.tsx
@@ -2,18 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { css } from "@/styled-system/css";
 import { Frontmatter } from "@/features/blog";
-
-function get_diff_date(date: Date) {
-  const diffMS = Date.now() - date.getTime();
-  const progress = new Date(diffMS);
-  const progressYear = progress.getUTCFullYear() - 1970;
-  const progressMonth = progress.getUTCMonth();
-  const progressDate = progress.getUTCDate() - 1;
-  if (progressYear) return progressYear === 1 ? "Last year" : `${progressYear} years ago`;
-  if (progressMonth) return progressMonth === 1 ? "Last month" : `${progressMonth} months ago`;
-  if (progressDate) return progressDate === 1 ? "Yesterday" : `${progressDate} days ago`;
-  return "Today";
-}
+import { LinkCardDateDiff } from "./LinkCardDateDiff";
 
 const card_style = css({
   display: "grid",
@@ -76,7 +65,9 @@ export const LinkCard = ({ slug, meta }: LinkCardProps) => {
           className={image_style}
         />
         <div className={text_container_style}>
-          <div className={date_style}>{get_diff_date(meta.date)}</div>
+          <div className={date_style}>
+            <LinkCardDateDiff date={meta.date} />
+          </div>
           <h2 className={title_style}>{meta.title}</h2>
           <div className={description_style}>{meta.description}</div>
         </div>
