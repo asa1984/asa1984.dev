1717909b1fb28007c9314869cf911035641645f7
asa1984 <satoasa9913@gmail.com>
2024-04-09 17:16:46 +0900

    4th grade -> 5th grade

--- packages/frontend/src/app/(routes)/profile/_components/Profile.tsx
+++ packages/frontend/src/app/(routes)/profile/_components/Profile.tsx
@@ -73,7 +73,7 @@ const ProfileCard = () => {
         <Info tag="Location" info="Miyagi, Japan" />
         <Info
           tag="Org"
-          info={"National Institue of Technology, Sendai College\n" + "Robotics course, 4th grade"}
+          info={"National Institue of Technology, Sendai College\n" + "Robotics course, 5th grade"}
         />
       </div>
       <div className={image_container_style}>
