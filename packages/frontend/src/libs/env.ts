2ab36ccdba3dabcc7ab7502d073c89943e4349d8
asa1984 <satoasa9913@gmail.com>
2024-12-01 18:01:44 +0900

    feat(deploy): Vercel -> Cloudflare Workers

--- packages/frontend/src/libs/env.ts
+++ packages/frontend/src/libs/env.ts
@@ -1,12 +1,21 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+// import { createEnv } from "@t3-oss/env-nextjs";
+// import { z } from "zod";
 
-export const env = createEnv({
-  server: {
-    NODE_ENV: z.union([z.literal("development"), z.literal("production"), z.literal("test")]),
-    BACKEND_API_TOKEN: z.string(),
-    FRONTEND_API_TOKEN: z.string(),
-    BACKEND_URL: z.string().url(),
-  },
-  experimental__runtimeEnv: process.env,
-});
+// export const env = createEnv({
+//   server: {
+//     BACKEND_URL: z.string().url(),
+//     BACKEND_API_TOKEN: z.string(),
+//     FRONTEND_URL: z.string().url(),
+//     FRONTEND_API_TOKEN: z.string(),
+//   },
+//   experimental__runtimeEnv: process.env,
+// });
+
+export type Env = Readonly<{
+  BACKEND_URL: string;
+  BACKEND_API_TOKEN: string;
+  FRONTEND_URL: string;
+  FRONTEND_API_TOKEN: string;
+}>;
+
+export const env = process.env as unknown as Env;
